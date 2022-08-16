60175dab3378b1b2ed3124c3a1e29755ab4358ad
Ashok <ashokksaini99@gmail.com>
2022-08-16 21:29:38 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path=require('path');
+const app = express();
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')); // For serving static files
+
+//ENDPOINTS
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+//STARTING SERVER
+app.listen(3000,(req,res)=>{
+    console.log("starting server...");
+});
